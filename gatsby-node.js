213b84c54a216bd091163e0f37e02c13b044f915
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-02-07 12:18:51 +0100

    Troubleshooting

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,7 @@
 
 const fs = require("fs");
 
-exports.createPages = ({ graphql, actions }) => {
+exports.createPages = ({ actions }) => {
   const { createPage } = actions;
   //const data = fs.readFileSync("./data/NA-LKM-S3-E.json");
   const data = fs.readFileSync("./data/metadata_med_undertyper.json");
@@ -14,7 +14,6 @@ function makePages(createPage, types) {
   Object.keys(types).forEach(kode => {
     const type = types[kode];
     //if (kode.startsWith("NN-LA-KLG"))
-    console.log(type.url);
     createPage({
       path: `/${type.url}/`,
       component: require.resolve("./src/templates/type.js"),
