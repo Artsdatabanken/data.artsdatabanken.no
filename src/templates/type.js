7b27dfcea44df3ae44a7143951669f87b363951b
helemork <helemork@gmail.com>
2019-03-20 13:14:45 +0100

    Added a basic header and styling for header items

--- src/templates/type.js
+++ src/templates/type.js
@@ -9,6 +9,7 @@ import DelAv from "./DelAv";
 import Relasjoner from "./Relasjoner";
 import Statistikk from "./Statistikk";
 import "./table.css";
+import "./style.css";
 
 export default props => {
   const {
@@ -16,20 +17,27 @@ export default props => {
   } = props;
   const tittel = Object.values(type.tittel)[0];
   return (
-    <div>
+    <div className="page_padding">
+      <div className="header_background">
+        <div className="header_padding">
+          <img src="https://artsdatabanken.no/Files/7809" className="top_image"/>
+          <h1 className="header_title">Artsdatabankens åpne data </h1>
+      </div>
+    </div>
       <Seo pageMeta={type} tittel={tittel} />
-      <div style={{ margin: "1rem" }}>
-        <img
+      <div>
+        {/*<img
           style={{ display: "block", float: "right", width: 204 }}
           src="https://data.artsdatabanken.no/Datakilde/Artsdatabanken/logo_med_navn_408.png"
-        />
-        <h2>{tittel} - Åpne data</h2>
-        <div style={{ paddingBottom: 12 }}>
+        />*/}
+        <h1>{tittel}</h1>
+        <div >
           {type.ingress} <a href={type.infoUrl}>{type.infoUrl}</a>
         </div>
+        
         <Bilde {...type.foto.forside} alt={"Foto av " + tittel.toLowerCase()} />
         <Statistikk tittel={tittel} {...type.stats} />
-        <div style={{ display: "flex", flexWrap: "wrap" }}>
+        <div>
           <DelAv overordnede={type.overordnet} />
           <Barna barn={type.barn} />
           <Relasjoner relasjoner={type.graf} />
