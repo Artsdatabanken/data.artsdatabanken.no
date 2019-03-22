e41c013d0d7648029c29a42bca83227ba0095c72
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-03-22 21:49:12 +0100

    Fix: Problem med å vise hovedsiden
    - Laster nå datafil fra data.artsdatabanken.no hvis den ikke ligger lokalt
    - Peker på separat mal for hovedsiden

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,38 +1,47 @@
 const fs = require("fs");
+const fetch = require("node-fetch");
+
+const dataUrl = "https://data.artsdatabanken.no/";
+const metadataFilename = "metadata_med_undertyper.json";
 
 exports.createPages = ({ actions }) => {
   const { createPage } = actions;
-  read("metadata_med_undertyper", createPage);
+  lesDatafil("Natur_i_Norge/Natursystem", createPage);
 };
 
-function read(fn, createPage) {
-  const data = fs.readFileSync("./data/" + fn + ".json");
+function lesDatafil(relUrl, createPage) {
+  const dataFilePath = "./data/" + relUrl.replace("/", "_") + ".json";
+  const url = dataUrl + relUrl + "/" + metadataFilename;
+  if (fs.existsSync(dataFilePath)) return read(dataFilePath, createPage);
+  fetch(url).then(response => {
+    if (response.status !== 200) throw new Error(response);
+    response.text().then(data => {
+      fs.writeFileSync(dataFilePath, data);
+      read(dataFilePath, createPage);
+    });
+  });
+}
+
+function read(dataFilePath, createPage) {
+  const data = fs.readFileSync(dataFilePath);
   let types = JSON.parse(data);
   if (types.data) types = types.data;
   if (types.tittel) types = { "~": types };
   makePages(createPage, types);
-  const rotdata = types["~"];
-  createPage({
-    path: `/`,
-    component: require.resolve("./src/templates/index.js"),
-    context:{rotdata},
-  });
 }
 
 function makePages(createPage, types) {
   Object.keys(types).forEach(kode => {
     const type = types[kode];
-    if (type.url.length > 190) {
-      console.log("2LONG", type.url);
-      return;
-    }
-    var url = type.url; //.replace("Natur_i_Norge/Landskap", "LA");
-    //url = url.replace("Natur_i_Norge/Natursystem", "NA");
     createPage({
-      path: `/${url}/`,
-      component: require.resolve("./src/templates/type.js"),
+      path: `/${type.url}/`,
+      component: finnTemplate(type),
       context: { type }
     });
-    
   });
+}
+
+function finnTemplate(type) {
+  if (type.kode === "~") return require.resolve("./src/templates/index.js");
+  return require.resolve("./src/templates/type.js");
 }
