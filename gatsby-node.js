19868eaa23ee3b9fa1de8e1bce9844f2d75eb3fd
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-03-27 10:49:26 +0100

    Les inndata fra https://nin.artsdatabanken.no

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,29 @@
 const fs = require("fs");
+const fetch = require("node-fetch");
+
+const dataUrl = "https://data.artsdatabanken.no/";
+const metadataFilename = "metadata_med_undertyper.json";
 
 exports.createPages = ({ actions }) => {
   const { createPage } = actions;
-  read("metadata_med_undertyper", createPage);
+  lesDatafil("Natur_i_Norge/Natursystem", createPage);
 };
 
-function read(fn, createPage) {
-  const data = fs.readFileSync("./data/" + fn + ".json");
+function lesDatafil(relUrl, createPage) {
+  const dataFilePath = "./data/" + relUrl.replace("/", "_") + ".json";
+  const url = dataUrl + relUrl + "/" + metadataFilename;
+  if (fs.existsSync(dataFilePath)) return read(dataFilePath, createPage);
+  fetch(url).then(response => {
+    if (response.status !== 200) throw new Error(response);
+    response.text().then(data => {
+      fs.writeFileSync(dataFilePath, data);
+      read(dataFilePath, createPage);
+    });
+  });
+}
+
+function read(dataFilePath, createPage) {
+  const data = fs.readFileSync(dataFilePath);
   let types = JSON.parse(data);
   if (types.data) types = types.data;
   if (types.tittel) types = { "~": types };
@@ -16,15 +33,15 @@ function read(fn, createPage) {
 function makePages(createPage, types) {
   Object.keys(types).forEach(kode => {
     const type = types[kode];
-    if (type.url.length > 190) {
-      console.log("2LONG", type.url);
-      return;
-    }
-    var url = type.url;
     createPage({
-      path: `/${url}/`,
-      component: require.resolve("./src/templates/type.js"),
+      path: `/${type.url}/`,
+      component: finnTemplate(type),
       context: { type }
-    });   
+    });
   });
+}
+
+function finnTemplate(type) {
+  if (type.kode === "~") return require.resolve("./src/templates/index.js");
+  return require.resolve("./src/templates/type.js");
 }
