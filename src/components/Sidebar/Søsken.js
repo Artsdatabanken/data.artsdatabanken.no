df01eaf506042362a126c51cfad82f9b0a6588f1
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-03-27 23:14:55 +0100

    Fix:   WebpackError: TypeError: Cannot read property 'length' of undefined

--- src/components/Sidebar/Søsken.js
+++ src/components/Sidebar/Søsken.js
@@ -1,42 +1,35 @@
 import React from "react";
 import Barna from "../../templates/Barna";
 
-const Søsken = ({ søsken,nåværende,barn }) => {
-  if (søsken.length <= 0) return null;
+const Søsken = ({ søsken, nåværende, barn }) => {
+  if (!søsken) return null;
   return (
     <React.Fragment>
-        {søsken.reverse().map(e => (
-          <Søskne nåværende={nåværende} barn={barn} key={e.kode} {...e} />
-        ))}
-      </React.Fragment>
+      {søsken.reverse().map(e => (
+        <Søskne nåværende={nåværende} barn={barn} key={e.kode} {...e} />
+      ))}
+    </React.Fragment>
   );
 };
 
-const Søskne = ({ kode, tittel, url, farge,nåværende,barn }) => {
+const Søskne = ({ kode, tittel, url, farge, nåværende, barn }) => {
   var tittel1 = Object.values(tittel)[0];
-  if(tittel1 ==="Katalog"){
+  if (tittel1 === "Katalog") {
     tittel1 = "Hovedsiden";
   }
   return (
     <React.Fragment>
-        <a href={url === "Katalog" ? "/" : `/${url}/index.html`}>
-        {tittel1!==nåværende &&
-          <li className="sidebar_link sidebar_sister_child">
-            {tittel1} 
-          </li>
-        }
-        {tittel1===nåværende &&
+      <a href={url === "Katalog" ? "/" : `/${url}/index.html`}>
+        {tittel1 !== nåværende && (
+          <li className="sidebar_link sidebar_sister_child">{tittel1}</li>
+        )}
+        {tittel1 === nåværende && (
           <React.Fragment>
-            <li className="sidebar_link sidebar_active_child">
-              {tittel1} 
-            </li>
-            <Barna barn={barn}/>
+            <li className="sidebar_link sidebar_active_child">{tittel1}</li>
+            <Barna barn={barn} />
           </React.Fragment>
-        }
-         
-              
-        </a>
-      
+        )}
+      </a>
     </React.Fragment>
   );
 };
