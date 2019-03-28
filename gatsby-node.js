624ac3791fc397cb3a728a0ee227ac076470b424
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-03-28 10:11:39 +0100

    Fix: WebpackError: TypeError: Cannot read property 'jsonName' of undefined

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,34 +8,56 @@ const isDeveloping = process.env.NODE_ENV === "development";
 
 exports.createPages = ({ actions }) => {
   const { createPage } = actions;
-  lesDatafil("Natur_i_Norge/Natursystem", createPage);
-  lesDatafil("Natur_i_Norge/Landskap", createPage);
-  lesDatafil("Biota", createPage);
-  lesDatafil("Fylke", createPage);
-  lesDatafil("Naturvernområde", createPage);
+  return new Promise((resolve, reject) => {
+    loadAll(createPage);
+    resolve();
+  });
 };
 
-function lesDatafil(relUrl, createPage) {
+async function loadAll(createPage) {
+  await lesDatafil("Natur_i_Norge/Natursystem", createPage);
+  await lesDatafil("Natur_i_Norge/Landskap", createPage);
+  await lesDatafil("Biota", createPage);
+  await lesDatafil("Fylke", createPage);
+  await lesDatafil("Naturvernområde", createPage);
+}
+
+async function lesDatafil(relUrl, createPage) {
   if (!fs.existsSync(dataPath)) fs.mkdirSync(dataPath);
   const dataFilePath = dataPath + relUrl.replace("/", "_") + ".json";
   const url = dataUrl + relUrl + "/" + metadataFilename;
   if (fs.existsSync(dataFilePath)) return read(dataFilePath, createPage);
   console.log("Downloading " + url);
-  fetch(url).then(response => {
-    if (response.status !== 200) throw new Error(response.status + " " + url);
-    response.text().then(data => {
-      fs.writeFileSync(dataFilePath, data);
-      read(dataFilePath, createPage);
-    });
-  });
+  const response = await fetch(url);
+  if (response.status !== 200) throw new Error(response.status + " " + url);
+  const data = await response.text();
+  fs.writeFileSync(dataFilePath, data);
+  read(dataFilePath, createPage);
 }
 
 function read(dataFilePath, createPage) {
   const data = fs.readFileSync(dataFilePath);
   let types = JSON.parse(data);
   if (types.data) types = types.data;
-  if (types.tittel) types = { "~": types };
+  Object.values(types).forEach(type => {
+    type.url = hack(type.url);
+    type.tittel.nb = hackNavn(type.tittel.nb);
+    const topindex = type.overordnet.length - 1;
+    if (topindex >= 0) {
+      const oo = type.overordnet;
+      oo[topindex].url = hack(oo[topindex].url);
+      oo[topindex].tittel.nb = hackNavn(oo[topindex].tittel.nb);
+    }
+  });
   makePages(createPage, types);
+}
+
+function hack(url) {
+  return url === "Katalog" ? "" : url;
+}
+
+function hackNavn(navn) {
+  return navn === "Katalog" ? "Åpne data" : navn;
 }
 
 function makePages(createPage, types) {
