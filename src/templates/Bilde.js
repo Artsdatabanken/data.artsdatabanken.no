9ea0854a0084fc2581c18fd233054a03a12e378b
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-03-29 11:57:15 +0100

    Vis bannerbilde bare hvis vi har et

--- src/templates/Bilde.js
+++ src/templates/Bilde.js
@@ -1,16 +1,19 @@
 import React from "react";
 
-const Bilde = ({ url, alt, lisens, opphav, utgiver }) => (
-  <div className="main_content_image_container">
-    <img
-      className="main_content_image"
-      src={url}
-      alt={alt}
-      onError={e => {
-        e.target.style.display = "none";
-      }}
-    />
-  </div>
-);
+const Bilde = ({ url, alt, lisens, opphav, utgiver }) => {
+  if (!url) return null;
+  return (
+    <div className="main_content_image_container">
+      <img
+        className="main_content_image"
+        src={url}
+        alt={alt}
+        onError={e => {
+          e.target.style.display = "none";
+        }}
+      />
+    </div>
+  );
+};
 
 export default Bilde;
