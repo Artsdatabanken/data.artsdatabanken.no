90001708566844f4af8eabb165f824b48c90d8c6
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-03-30 14:33:19 +0100

    Logo inn i repo
    Minimaliser avhengighet til eksterne servere i produksjon

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ class Header extends Component {
       <div className="header_background">
         <div className="header_padding">
           <img
-            src="https://artsdatabanken.no/Files/7809"
+            src="/logo.png"
             className="top_image"
             alt="artsdatabanken logo"
           />
