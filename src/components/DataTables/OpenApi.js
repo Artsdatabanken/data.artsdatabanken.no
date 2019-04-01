880e902e7e37c8756cb6cf91c4b8c09b572c02d3
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-04-01 22:32:17 +0200

    Forstår ikke lenkene til Artskart
    Når de virker, hvorfor.  Hvordan få ut JSON?

--- src/components/DataTables/OpenApi.js
+++ src/components/DataTables/OpenApi.js
@@ -5,7 +5,7 @@ const arter = [
   {
     tittel: "Artskart: Observasjoner",
     url:
-      "https://artskart.artsdatabanken.no/appapi/api/data/SearchLocations?&TaxonIds%5B%5D=%ID%&TaxonGroupIds%5B%5D=&IncludeSubTaxonIds=true&Months%5B%5D=&Categories%5B%5D=&BasisOfRecords%5B%5D=&Behaviors%5B%5D=&InstitutionIds%5B%5D=&CollectionIds%5B%5D=&Img%5B%5D=&Found%5B%5D=&NotRecovered%5B%5D=&Valid%5B%5D=&UnsureId%5B%5D=&Spontan%5B%5D=&CenterPoints=false&EpsgCode=32633&LocationId=0&GroupBy=0&Style=1&YearFrom=0&YearTo=0&CoordinatePrecisionFrom=0&CoordinatePrecisionTo=0&BoundingBox=",
+      "https://artskart.artsdatabanken.no/appapi/api/data/SearchLocations?&TaxonIds%5B%5D=%ID%&IncludeSubTaxonIds=true",
     protokoll: "REST JSON",
     beskrivelse: "Lokaliteter hvor arten er observert"
   },
