40b62d9b0069162b657abe7760f383232cdb43b0
Even <even.juberg@ntnu.no>
2019-04-04 16:10:20 +0200

    Deployment is fun, yes?

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ tar -czf data-portal.tar.gz public/*.html deploy-www.js
 ls -la data-portal.tar.gz
 echo Deploying...
 sshpass -p $scp_pass scp -o StrictHostKeyChecking=no data-portal.tar.gz $scp_user@158.38.128.51:/tmp
-curl -X POST --data-urlencode "payload={\"channel\": \"$slack_user\", \"username\": \"travis not the band\", \"text\": \"slack_command\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/$SLACK_TOKEN
+curl -X POST --data-urlencode "payload={\"channel\": \"$slack_chan\", \"username\": \"travis not the band\", \"text\": \"$slack_command\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/$SLACK_TOKEN
