5019db2997193bc52492dd842826df9bbed662d2
Even <even.juberg@ntnu.no>
2019-04-08 14:13:17 +0200

    Added functionality for conditional deployment, based on branches.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-echo Making archive...
+if [ "$1" != "master" ]
+then
+echo "Making archive..."
+tar -czf $1.tar.gz public/*.html deploy-www.js
+echo "Deploying..."
+sshpass -p $scp_pass scp -o StrictHostKeyChecking=no $1.tar.gz $scp_user@158.38.128.51:/tmp/dataportal
+curl -X POST --data-urlencode "payload={\"channel\": \"$2\", \"username\": \"travis not the band\", \"text\": \"$3\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/$SLACK_TOKEN
+else 
 tar -czf data-portal.tar.gz public/*.html deploy-www.js
-ls -la data-portal.tar.gz
-echo Deploying...
-sshpass -p $scp_pass scp -o StrictHostKeyChecking=no data-portal.tar.gz $scp_user@158.38.128.51:/tmp
-curl -X POST --data-urlencode "payload={\"channel\": \"$slack_chan\", \"username\": \"travis not the band\", \"text\": \"$slack_command\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/$SLACK_TOKEN
+sshpass -p $scp_pass scp -o StrictHostKeyChecking=no data-portal.tar.gz $scp_user@158.38.128.51:/tmp/dataportal
+curl -X POST --data-urlencode "payload={\"channel\": \"$2\", \"username\": \"travis not the band\", \"text\": \"$3\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/$SLACK_TOKEN
+fi
