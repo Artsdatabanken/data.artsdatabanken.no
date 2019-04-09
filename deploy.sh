04865d33634bd0188ba39c63cd25c6d3a581f6cb
Even <even.juberg@ntnu.no>
2019-04-09 08:38:55 +0200

    Simplifying deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-if [ "$1" != "master" ]
-then
+#Please add variables as environmental variables in Travis settings for the project when using this script.
+#These lines should also be added in the "script block" of travis.yml.
+#- export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
+#- echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
+BRANCH=$1
 echo "Making archive..."
-tar -czf $1.tar.gz public/ deploy-www.js
+tar -czf $BRANCH.tar.gz public/ deploy-www.js
 echo "Deploying..."
-sshpass -p $scp_pass scp -o StrictHostKeyChecking=no $1.tar.gz $scp_user@158.38.128.51:/tmp/dataportal
-curl -X POST --data-urlencode "payload={\"channel\": \"$2\", \"username\": \"travis not the band\", \"text\": \"$3\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/$SLACK_TOKEN
-else 
-tar -czf data-portal.tar.gz public deploy-www.js
-sshpass -p $scp_pass scp -o StrictHostKeyChecking=no data-portal.tar.gz $scp_user@158.38.128.51:/tmp/dataportal
-curl -X POST --data-urlencode "payload={\"channel\": \"$2\", \"username\": \"travis not the band\", \"text\": \"$3\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/$SLACK_TOKEN
-fi
+sshpass -p $scp_pass scp -o StrictHostKeyChecking=no $BRANCH.tar.gz $scp_user@$scp_dest
+#Posting to slack to trigger deployment
+curl -X POST --data-urlencode "payload={\"channel\": \"$slack_chan\", \"username\": \"travis not the band\", \"text\": \"$slack_command\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/$SLACK_TOKEN
