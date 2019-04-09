5f4d6e365d96904cb8b2d58d0c1a90739d60e0b1
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-04-09 13:43:45 +0200

    Last ned datafiler i prebuild

---
+++ lastNedData.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const fetch = require("node-fetch");
+const {
+  dataPath,
+  dataUrl,
+  metadataFilename,
+  getLocalDataFilePathForUrl
+} = require("./config");
+
+lastNedFiler();
+
+async function lastNedFiler() {
+  await lesUrl("Natur_i_Norge/Natursystem");
+  await lesUrl("Natur_i_Norge/Landskap");
+  await lesUrl("Biota");
+  await lesUrl("Fylke");
+  await lesUrl("Naturvernområde");
+  await lesUrl("Datakilde");
+  await lesUrl("Truet_art_natur");
+  await lesUrl("", "index.json");
+}
+
+async function lesUrl(relUrl, file = metadataFilename) {
+  if (!fs.existsSync(dataPath)) {
+    console.log("Creating " + dataPath);
+    fs.mkdirSync(dataPath);
+  }
+  const relativePath = relUrl ? relUrl + "/" : "";
+  const url = dataUrl + relativePath + metadataFilename;
+  const filePath = getLocalDataFilePathForUrl(relUrl, file);
+  if (fs.existsSync(filePath)) return filePath;
+  console.log("Downloading " + url + " til " + filePath);
+  const response = await fetch(url);
+  if (response.status !== 200) throw new Error(response.status + " " + url);
+  const data = await response.text();
+  fs.writeFileSync(filePath, data);
+  console.log("Wrote", filePath);
+  return filePath;
+}
