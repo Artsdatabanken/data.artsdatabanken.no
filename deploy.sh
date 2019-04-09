bb888348403d5ce072f902240c675824725a9f7d
Even <even.juberg@ntnu.no>
2019-04-09 14:23:05 +0200

    Pushing after rebasing from master.

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 #- export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 #- echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
 BRANCH=$1
-echo "Making archive..."
+echo "Making archive...."
 tar -czf $BRANCH.tar.gz public/ deploy-www.js
 echo "Deploying..."
 sshpass -p $scp_pass scp -o StrictHostKeyChecking=no $BRANCH.tar.gz $scp_user@$scp_dest
