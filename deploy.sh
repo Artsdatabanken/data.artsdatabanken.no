3b80b5ef11fd99ded8888c25d2b687ac4564288e
Even <even.juberg@ntnu.no>
2019-04-10 11:00:02 +0200

    Trying to figure posting to PR in git from Travis.

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@
 #- export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 #- echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
 echo $BRANCH
+echo $TRAVIS_REPO_SLUG
 BRANCH=$1
 echo "Making archive..."
 tar -czf $BRANCH.tar.gz public/ deploy-www.js
@@ -11,3 +12,8 @@ echo "Deploying..."
 sshpass -p $scp_pass scp -o StrictHostKeyChecking=no $BRANCH.tar.gz $scp_user@$scp_dest
 #Posting to slack to trigger deployment
 curl -X POST --data-urlencode "payload={\"channel\": \"$slack_chan\", \"username\": \"travis not the band\", \"text\": \"$slack_command\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/$SLACK_TOKEN
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]
+then
+echo "Posting to github ..."
+curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"Magic\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+fi
