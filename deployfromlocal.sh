c3fa24eba0623bc74614b08ef5f72ecc1bd97d2c
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-04-11 11:04:44 +0200

    Remove deployment hacks
    Strategy: Patch Gatsby

--- deployfromlocal.sh
+++ deployfromlocal.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
+DESTPATH=/home/grunnkart/
 echo Making archive...
-scp -r public/static grunnkart@hydra:~/tilesdata/
-tar -czf data-portal.tar.gz public/* deploy-www.js
-ls -la data-portal.tar.gz
+tar -cjf data-portal.tar.gz -C ./public .
 echo Deploying...
 scp data-portal.tar.gz grunnkart@nin.artsdatabanken.no:~/
-ssh grunnkart@hydra tar -zxf data-portal.tar.gz
-ssh grunnkart@hydra node deploy-www.js metabase.json
-scp public/*.js grunnkart@hydra:~/tilesdata/
-scp public/index.html grunnkart@hydra:~/tilesdata/
+ssh grunnkart@hydra tar -xjf data-portal.tar.gz -C ./tilesdata
