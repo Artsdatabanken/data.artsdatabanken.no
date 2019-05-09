ed122e98647e68df5e65a69d670136952d27adea
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-05-09 15:02:24 +0200

    Klikk på bilde for opprinnelse

--- src/templates/Bilde.js
+++ src/templates/Bilde.js
@@ -1,13 +1,16 @@
 import React from "react";
 
-const Bilde = ({ url, alt, lisens, opphav, utgiver }) => {
+const Bilde = ({ url, kildeurl }) => {
   if (!url) return null;
   return (
     <div className="main_content_image_container">
       <img
         className="main_content_image"
         src={url}
-        alt={alt}
+        style={{ cursor: "pointer" }}
+        onClick={() => {
+          if (kildeurl) window.location = kildeurl;
+        }}
         onError={e => (e.target.style.display = "none")}
       />
     </div>
