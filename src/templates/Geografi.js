91bff374454bea204de30d996645783c689334bb
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-07-01 11:37:41 +0200

    Add preview image

--- src/templates/Geografi.js
+++ src/templates/Geografi.js
@@ -1,21 +1,35 @@
 import React from "react";
 
 const Geografi = ({ url, children }) => {
-  const previewUrl = `https://data.artsdatabanken.no/${url}/thumbnail.32633.blur.png`;
+  const previewUrl = `https://data.artsdatabanken.no/${url}/thumbnail.png`;
   return (
     <div className="table" style={{}}>
       <h2>Geografi</h2>
       {children}
-      <h3>Utbredelse</h3>
+      <h3>Forhåndsvisning</h3>
       <div className="imagecontainer">
         <a href={"https://nin.artsdatabanken.no/" + url}>
-          <img
-            src="https://data.artsdatabanken.no/Basiskart/NaturalEarth/bak_liten_grå.32633.png"
-            alt="bakgrunnskart"
-          />
-          <img src={previewUrl} className="imageFilter" alt="dekningskart" />
+          <img src={previewUrl} alt="kartvisning" />
         </a>
       </div>
+      {false && (
+        <>
+          <h3>Utbredelse</h3>
+          <div className="imagecontainer">
+            <a href={"https://nin.artsdatabanken.no/" + url}>
+              <img
+                src="https://data.artsdatabanken.no/Basiskart/NaturalEarth/bak_liten_grå.32633.png"
+                alt="bakgrunnskart"
+              />
+              <img
+                src={previewUrl}
+                className="imageFilter"
+                alt="dekningskart"
+              />
+            </a>
+          </div>
+        </>
+      )}
     </div>
   );
 };
