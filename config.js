45e83b87d3b2a22f31b23db2c650e808182d1240
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-08-21 10:58:20 +0200

    Remove unused import

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 const dataUrl = "https://data.artsdatabanken.no/";
 const dataPath = "./data/";
 const metadataFilename = "metadata_med_undertyper.json";
-const path = require("path");
 
 function getLocalDataFilePathForUrl(relUrl, filename) {
   const prefix = relUrl ? relUrl.replace(/\//g, "_") + "_" : "";
