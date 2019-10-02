ac079aad7d662b41d1111846a3a18fe65265e7dd
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-10-02 09:29:50 +0200

    Add front page

--- App.js
+++ App.js
@@ -6,11 +6,11 @@ import {
   query
 } from "@phenomic/preset-react-app/lib/client";
 import Type from "./src/templates/type";
+import FrontPage from "./src/pages/index";
 
 const Test = props => {
   if (!props.page.node) return null;
   const node = props.page.node;
-  console.error("node", node);
   return <Type pageContext={node} />;
 };
 
@@ -20,6 +20,7 @@ const TestContainer = withPhenomicApi(Test, props => ({
 
 export default createApp(() => (
   <Router history={browserHistory}>
+    <Route path="/" component={FrontPage} />
     <Route path="/*" component={TestContainer} />
   </Router>
 ));
